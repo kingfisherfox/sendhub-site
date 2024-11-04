0bbfe72d96e8189a1cdf22ec3174e295c4f54a70
Benjamin Powell <me@benjaminspowell.com>
2024-11-04 10:38:38 +0700

    updated link

--- src/components/SingleCardPricing.jsx
+++ src/components/SingleCardPricing.jsx
@@ -47,7 +47,7 @@ const SingleCardPricing = () => {
                     <span className="text-5xl font-semibold tracking-tight text-gray-900">$0</span>
                     <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">USD</span>
                     </p>
-                    <a href="#" className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                    <a href="https://chromewebstore.google.com/detail/sendhub/cggdlflcedinlcmcnniblbndckpmegae?authuser=1&hl=en-AU" className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                     Get access
                     </a>
 
